9f6a87b9b7a7c71402aed8209ec0f42b29ff798a
Ashish Soni <ashjas@gmail.com>
2015-07-23 20:11:32 +0530

    Incorporate review comments.

--- app/src/main/java/com/ashjas/myappportfolio/MainActivity.java
+++ app/src/main/java/com/ashjas/myappportfolio/MainActivity.java
@@ -10,11 +10,13 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import android.widget.Button;
 import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
 
+	private Toast mAppToast;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -25,53 +27,13 @@ public class MainActivity extends ActionBarActivity {
 					.commit();
 		}
 	}
+
 	public void onButtonClick(View view)
 	{
-		switch(view.getId())
-		{
-			case R.id.Buttonspotify:
-				Toast.makeText(this,R.string.SpotifyStremer,Toast.LENGTH_SHORT).show();
-				break;
-			case R.id.Buttonscores:
-				Toast.makeText(this,R.string.ScoresApp,Toast.LENGTH_SHORT).show();
-				break;
-			case R.id.Buttonlibrary:
-				Toast.makeText(this,R.string.LibraryApp,Toast.LENGTH_SHORT).show();
-				break;
-			case R.id.ButtonBuildItBigger:
-				Toast.makeText(this,R.string.BuildItBigger,Toast.LENGTH_SHORT).show();
-				break;
-			case R.id.ButtonXYZ:
-				Toast.makeText(this,R.string.XYZReader,Toast.LENGTH_SHORT).show();
-				break;
-			case R.id.ButtonCapstone:
-				Toast.makeText(this,R.string.Capstone,Toast.LENGTH_SHORT).show();
-				break;
-		}
-
-	}
-
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.menu_main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-
-		//noinspection SimplifiableIfStatement
-		if (id == R.id.action_settings) {
-			return true;
-		}
-
-		return super.onOptionsItemSelected(item);
+		if(mAppToast != null)
+			mAppToast.cancel();
+		mAppToast =	Toast.makeText(this,((Button)view).getText().toString(),Toast.LENGTH_SHORT);
+		mAppToast.show();
 	}
 
 	/**
